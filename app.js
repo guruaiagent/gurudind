0f367c6289cabcbe71dbc456b8256b5ae1cc73a3
Gurushantha <gurushantha.doddamani@sap.com>
2026-03-06 07:42:02 +0530

    Devops message

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to iQuant Home Page!');
+  res.send('Welcome to Devops World!');
 });
 
 app.get('/hello', (req, res) => {
-  res.send('Hello, World! Welcome to iQuant YouTube Channel.\n');
+  res.send('Hello, DevOps Engineers! Welcome to CI/CD Process.\n');
 });
 
 app.listen(port, () => {
